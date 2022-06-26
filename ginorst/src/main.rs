73ccca0ea5ac051fe9bda8068dc1bcb33c31945a
Harmy1410 <harmy1410@gmail.com>
2022-06-26 00:28:07 -0400

    adding print in sort function just because

--- ginorst/src/main.rs
+++ ginorst/src/main.rs
@@ -2,6 +2,6 @@ mod insertion;
 
 fn main() {
     let mut unsorted = vec![5, 4, 3, 2, 1];
-    let sorter = insertion::Sort::new(&mut unsorted);
-    sorter.print();
+    let mut sorter = insertion::Sort::new(&mut unsorted);
+    sorter.sort();
 }
