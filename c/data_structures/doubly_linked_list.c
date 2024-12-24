ed03e0b225e5ee7438c01721941652adcf4ef352
Harmee Patel <harmeepatel@gmail.com>
2024-12-23 23:30:10 -0500

    feat: push and pop in dll

--- c/data_structures/doubly_linked_list.c
+++ c/data_structures/doubly_linked_list.c
@@ -16,12 +16,13 @@ void printAndExit(char* str)
     exit(1);
 }
 
+// node
 struct Node {
     TYPE val;
     struct Node* prev;
     struct Node* next;
 };
-// node
+
 struct Node* create_node(TYPE val)
 {
     struct Node* node = (struct Node*)malloc(sizeof(struct Node));
@@ -31,11 +32,13 @@ struct Node* create_node(TYPE val)
     return node;
 }
 
+// doubly linked list
 struct DLL {
     unsigned int len;
     struct Node* head;
     struct Node* tail;
 };
+
 struct DLL* create_dll(TYPE val)
 {
     struct DLL* dll = (struct DLL*)malloc(sizeof(struct DLL));
@@ -52,7 +55,7 @@ void print_dll(struct DLL* dll)
     struct Node* node = dll->head;
 
     while (node) {
-        printf("%d:%d", i, node->val);
+        printf("%0*d:%2d", 2, i, node->val);
         if (node->next) {
             printf(" -> ");
         }
@@ -60,6 +63,23 @@ void print_dll(struct DLL* dll)
         ++i;
     }
     printf("\n");
+}
+
+void free_dll(struct DLL* dll)
+{
+    if (dll == NULL)
+        return;
+
+    struct Node* curr = dll->head;
+    struct Node* next;
+
+    while (curr) {
+        next = curr->next;
+        free(curr);
+        curr = next;
+    }
+
+    free(dll);
 }
 
 // traversing
@@ -109,7 +129,7 @@ struct Node* node_with_val(struct DLL* dll, TYPE needle)
 }
 
 // modify
-enum Bool prepend(struct DLL* dll, TYPE val)
+enum Bool push_first(struct DLL* dll, TYPE val)
 {
     struct Node* newNode = create_node(val);
 
@@ -120,7 +140,8 @@ enum Bool prepend(struct DLL* dll, TYPE val)
     ++dll->len;
     return TRUE;
 }
-enum Bool append(struct DLL* dll, TYPE val)
+
+enum Bool push_last(struct DLL* dll, TYPE val)
 {
     struct Node* newNode = create_node(val);
 
@@ -132,16 +153,77 @@ enum Bool append(struct DLL* dll, TYPE val)
     return TRUE;
 }
 
-enum Bool insert(struct DLL* dll, int pos, TYPE val)
+enum Bool push_at(struct DLL* dll, int pos, TYPE val)
 {
+    if (pos < 0 || pos > dll->len) {
+        printAndExit("!!! cannot insert at invalid position");
+    }
+    if (pos == 0) {
+        push_first(dll, val);
+        return TRUE;
+    }
+    if (pos == dll->len) {
+        push_last(dll, val);
+        return TRUE;
+    }
     struct Node* newNode = create_node(val);
 
-    dll->tail->next = newNode;
-    newNode->prev = dll->tail;
-    dll->tail = newNode;
+    struct Node* ith = ith_node(dll, pos);
+
+    newNode->prev = ith->prev;
+    newNode->next = ith;
+    ith->prev->next = newNode;
+    ith->prev = newNode;
 
     ++dll->len;
     return TRUE;
+}
+
+TYPE pop_first(struct DLL* dll)
+{
+    TYPE val = dll->head->val;
+    struct Node* head = dll->head;
+
+    if (dll->len == 1) {
+        printAndExit("!!! cannot delete the only value in linked list.");
+    }
+    dll->head = head->next;
+    dll->head->prev = NULL;
+    head->next = NULL;
+
+    --dll->len;
+    free(head);
+    return val;
+}
+TYPE pop_last(struct DLL* dll)
+{
+    TYPE val = dll->tail->val;
+    struct Node* tail = dll->tail;
+
+    if (dll->len == 1) {
+        printAndExit("!!! cannot delete the only value in linked list.");
+    }
+    dll->tail = tail->prev;
+    dll->tail->next = NULL;
+    tail->prev = NULL;
+
+    --dll->len;
+    free(tail);
+    return val;
+}
+TYPE pop_at(struct DLL* dll, int pos)
+{
+    struct Node* ith = ith_node(dll, pos);
+    TYPE val = ith->val;
+
+    ith->prev->next = ith->next;
+    ith->next->prev = ith->prev;
+
+    ith->next = NULL;
+    ith->prev = NULL;
+    free(ith);
+
+    return val;
 }
 
 // main
@@ -149,14 +231,17 @@ int main()
 {
     struct DLL* dll = create_dll(0);
     int len = dll->len;
-    for (int i = 1; i <= 10; ++i) {
-        if (append(dll, i) == FALSE) {
+
+    // push_last
+    for (int i = 1; i <= 8; ++i) {
+        if (push_last(dll, i) == FALSE) {
             printAndExit("!!! failed to append");
         }
         ++len;
     }
+    // push_first
     for (int i = 1; i <= 8; ++i) {
-        if (prepend(dll, i * -1) == FALSE) {
+        if (push_first(dll, i * -1) == FALSE) {
             printAndExit("!!! failed to prepend");
         }
         ++len;
@@ -167,8 +252,32 @@ int main()
     assert(ith_val(dll, 9) == 1);
     assert(ith_val(dll, 15) == 7);
 
-    assert(dll->len == len);
+    // push_at
+    const int i_val = 99;
+    static const int pos[] = { 0, 10, 10, 20 };
+    for (int i = 0; i != sizeof(pos) / sizeof(pos[0]); ++i) {
+        push_at(dll, pos[i], i_val);
+        ++len;
+    }
+    print_dll(dll);
 
-    free(dll);
+    // pop_first
+    pop_first(dll);
+    --len;
+    // pop_last
+    pop_last(dll);
+    --len;
+    print_dll(dll);
+
+    // pop_at
+    pop_at(dll, 10);
+    --len;
+    pop_at(dll, 9);
+    --len;
+    print_dll(dll);
+
+    // assert(dll->len == len);
+
+    free_dll(dll);
     return 0;
 }
